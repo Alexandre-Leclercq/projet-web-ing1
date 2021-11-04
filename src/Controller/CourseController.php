8cf5805cc59b584f2d9187e37f48c72556a02581
Alexandre-Leclercq <69433685+Alexandre-Leclercq@users.noreply.github.com>
2021-11-04 17:10:42 +0100

    start to customise bootstrap

---
+++ src/Controller/CourseController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CourseController extends AbstractController
+{
+    /**
+     * @Route("/course/list", name="listCourse")
+     */
+    public function list(): Response
+    {
+        return $this->render('course/index.html.twig');
+    }
+
+    /**
+     * @Route("/course/edit", name="editCourse")
+     */
+    public function edit(): Response
+    {
+        return $this->render('course/index.html.twig');
+    }
+
+    /**
+     * @Route("/course/add", name="addCourse")
+     */
+    public function add(): Response
+    {
+        return $this->render('course/index.html.twig');
+    }
+}
