2123413898e44213a7a0769708d1d895a5bb6c7b
Alexandre-Leclercq <69433685+Alexandre-Leclercq@users.noreply.github.com>
2021-12-24 14:20:19 +0100

    add page myCourse

--- src/Controller/User/IndexController.php
+++ src/Controller/User/IndexController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\User;
 
 use App\Repository\UserRepository;
 use App\Repository\CategoryRepository;
+use App\Repository\CourseStatusRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,11 +28,17 @@ class IndexController extends AbstractController
      */
     private $em;
 
-    public function __construct(Security $security, CategoryRepository $categoryRepository, ManagerRegistry $managerRegistry)
+    /**
+     * @var CourseStatusRepository
+     */
+    private $courseStatusRepository;
+
+    public function __construct(Security $security, CategoryRepository $categoryRepository, ManagerRegistry $managerRegistry, CourseStatusRepository $courseStatusRepository)
     {
         $this->security = $security;
         $this->categoryRepository = $categoryRepository;
         $this->em = $managerRegistry->getManager();
+        $this->courseStatusRepository = $courseStatusRepository;
     }
 
     /**
@@ -41,8 +48,7 @@ class IndexController extends AbstractController
     {
         //global
         $user = $this->security->getUser(); // get current user login
-
-        if (is_null($user)) {
+        if(is_null($user)) {
             return $this->render('index.html.twig');
         } else {
             $user = $userRepository->find($user->getIdUser());
@@ -50,9 +56,15 @@ class IndexController extends AbstractController
             $this->em->persist($user);
             $this->em->flush();
             $this->em->clear();
+
+            $lastCourse = $this->courseStatusRepository->findOneBy(['idUser' => $user], ['lastDatetime' => 'DESC']);
+            $starredCourses = $this->courseStatusRepository->findBy(['idUser' => $user, 'starred' => true], ['lastDatetime' => 'DESC']);
+
             return $this->render('user/index.html.twig', [
                 'user' => $this->security->getUser(),
-                'categories' => $this->categoryRepository->findBy(['active' => true])
+                'categories' => $this->categoryRepository->findBy(['active' => true]),
+                'lastCourse' => $lastCourse,
+                'starredCourses' => $starredCourses
             ]);
         }
     }
